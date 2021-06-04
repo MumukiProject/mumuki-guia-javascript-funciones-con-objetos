d6f9b4b11a15f46e8cd39c72d336ac2836479293
mayra@mumuki.org <mayra@mumuki.org>
2021-06-04 20:50:15 +0000

    Mumuki Export on 2021-06-04 20:50:15 +0000

--- 00001_Nombre completo/test..js
+++ 00001_Nombre completo/test..js
@@ -1,8 +1,7 @@
 /*describe("", function() {
-  it("Si el nombre es Juli y la edad es 48, retorna Hola, mi nombre es Juli y tengo 48 años", function() {
+  it("Si ", function() {
     perfil.nombre = 'Juli';
     perfil.edad = 48;
     assert.equal(perfil.presentacion(),'Hola, mi nombre es Juli y tengo 48 años');
   })
-})
-*/
+})*/
