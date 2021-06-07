ffd290fdeee8f4564e1b493a0eb231477df12022
mayra@mumuki.org <mayra@mumuki.org>
2021-06-07 18:58:28 +0000

    Mumuki Export on 2021-06-07 18:58:28 +0000

--- 00012_Parámetros de búsqueda/test..js
+++ 00012_Parámetros de búsqueda/test..js
@@ -1,28 +1,28 @@
 describe("", function() {
-  it("Si hacemos  ('http://www.exercises.com?keyword=objects&language=javascript&level=intermediate') el keyword es objects", function() {
+  it("Si hacemos parseQueryParams('http://www.exercises.com?keyword=objects&language=javascript&level=intermediate') el keyword es objects", function() {
     let resultado =  parseQueryParams("http://www.exercises.com?keyword=objects&language=javascript&level=intermediate");
     assert.equal(resultado.keyword, 'objects');
   })
-  it("Si hacemos  ('http://www.exercises.com?keyword=objects&language=javascript&level=intermediate') el language es javascript", function() {
+  it("Si hacemos parseQueryParams('http://www.exercises.com?keyword=objects&language=javascript&level=intermediate') el language es javascript", function() {
     let resultado =  parseQueryParams("http://www.exercises.com?keyword=objects&language=javascript&level=intermediate");
     assert.equal(resultado.language, 'javascript');
   })
-  it("Si hacemos  ('http://www.exercises.com?keyword=objects&language=javascript&level=intermediate') el level es intermediate", function() {
+  it("Si hacemos parseQueryParams('http://www.exercises.com?keyword=objects&language=javascript&level=intermediate') el level es intermediate", function() {
     let resultado =  parseQueryParams("http://www.exercises.com?keyword=objects&language=javascript&level=intermediate");
     assert.equal(resultado.level, 'intermediate');
   })
 });
 
 describe("", function() {
-  it("Si hacemos  ('http://www.exercises.com?keyword=functional&language=haskell&level=advanced') el keyword es objects", function() {
+  it("Si hacemos parseQueryParams('http://www.exercises.com?keyword=functional&language=haskell&level=advanced') el keyword es objects", function() {
     let resultado =  parseQueryParams("http://www.exercises.com?keyword=functional&language=haskell&level=advanced");
     assert.equal(resultado.keyword, 'functional');
   })
-  it("Si hacemos  ('http://www.exercises.com?keyword=functional&language=haskell&level=advanced') el language es haskell", function() {
+  it("Si hacemos parseQueryParams('http://www.exercises.com?keyword=functional&language=haskell&level=advanced') el language es haskell", function() {
     let resultado =  parseQueryParams("http://www.exercises.com?keyword=functional&language=haskell&level=advanced");
     assert.equal(resultado.language, 'haskell');
   })
-  it("Si hacemos  ('http://www.exercises.com?keyword=functional&language=haskell&level=advanced') el level es advanced", function() {
+  it("Si hacemos parseQueryParams('http://www.exercises.com?keyword=functional&language=haskell&level=advanced') el level es advanced", function() {
     let resultado =  parseQueryParams("http://www.exercises.com?keyword=functional&language=haskell&level=advanced");
     assert.equal(resultado.level, 'advanced');
   })
