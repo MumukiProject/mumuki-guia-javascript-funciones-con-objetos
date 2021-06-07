919b99187800b69b4f080f81aead38b5e268a5e0
mayra@mumuki.org <mayra@mumuki.org>
2021-06-07 20:05:55 +0000

    Mumuki Export on 2021-06-07 20:05:55 +0000

--- 00016_Diferencia de objetos/description.md
+++ 00016_Diferencia de objetos/description.md
@@ -1,7 +1,10 @@
-- Igual que el anterior, pero debe devolver un objeto con las propiedades que no se repiten entre ambos objetos
+Similar al ejercicio anterior, pero debe devolver un objeto con las propiedades que no se repiten entre ambos objetos.
 
 ```javascript
 const info1 = { a: 1, b: 2, c: 3 };
 const info2 = { d: 4, b: 5 };
-differentiate(info1, info2); // returns {a: 1, c: 3, d: 4}
+ム diferencia(info1, info2);
+{a: 1, c: 3, d: 4}
 ```
+
+> Definí la función `diferencia` con el comportamiento que acabamos de describir.
--- 00016_Diferencia de objetos/test..js
+++ 00016_Diferencia de objetos/test..js
@@ -1,1 +1,17 @@
-TODO
+describe("", function() {
+  it("Si hacemos diferencia({ a: 1, b: 2, c: 3 }, { d: 4, b: 5 })", function() {
+   let info1 = { a: 1, b: 2, c: 3 };
+   let info2 = { d: 4, b: 5 };
+   let resultado = diferencia(info1, info2)
+    assert.equal(resultado, {a: 1, c: 3, d: 4});
+  })
+});
+
+describe("", function() {
+  it("Si hacemos diferencia({ a: 4, b: 8, c: 5 }, { h: 4, b: 5 })", function() {
+   let info1 = { a: 4, b: 8, c: 5 };
+   let info2 = { h: 4, b: 5 };
+   let resultado = diferencia(info1, info2)
+    assert.equal(resultado, {a: 4, c: 5, h: 4});
+  })
+});
