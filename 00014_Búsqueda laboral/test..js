8cfb6d8f24d0403e2e642797f9400c3199b0e7be
mayra@mumuki.org <mayra@mumuki.org>
2021-06-07 19:24:28 +0000

    Mumuki Export on 2021-06-07 19:24:28 +0000

--- 00014_Búsqueda laboral/test..js
+++ 00014_Búsqueda laboral/test..js
@@ -1,3 +1,39 @@
-Si una persona cumple con todos los requisitos de una búsqueda, se adecua a la misma.
+describe("", function() {
+  it("Si una persona cumple con todos los requisitos de una búsqueda, se adecua a la misma", function() {
+   let persona = {
+        experiencia: 4,
+        lenguajes: ["JavaScript", "HTML","CSS"],
+        locacion: ["Buenos Aires"],
+        remuneracion: 35000
+      }
 
-Si una persona no cuenta con los años de experiencia necesarios de una búsqueda, no se adecua.
+    let busqueda = {
+      experiencia: 1,
+      lenguajes: ["JavaScript", "HTML"],
+      locacion: ["Buenos Aires"],
+      remuneracion: 40000
+    }
+
+    assert(seAdecua(persona, busqueda));
+  })
+});
+
+describe("", function() {
+  it("Si una persona no cuenta con los años de experiencia necesarios de una búsqueda, no se adecua", function() {
+   let persona = {
+        experiencia: 1,
+        lenguajes: ["JavaScript", "HTML","CSS"],
+        locacion: ["Buenos Aires"],
+        remuneracion: 35000
+      }
+
+    let busqueda = {
+      experiencia: 4 ,
+      lenguajes: ["JavaScript", "HTML"],
+      locacion: ["Buenos Aires"],
+      remuneracion: 40000
+    }
+
+    assert.not(seAdecua(persona, busqueda));
+  })
+});
