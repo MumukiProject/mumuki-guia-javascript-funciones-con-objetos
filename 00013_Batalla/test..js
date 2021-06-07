03dcc7325f1cb81bcbd6d08ef29682dd126341f1
mayra@mumuki.org <mayra@mumuki.org>
2021-06-07 19:26:16 +0000

    Mumuki Export on 2021-06-07 19:26:16 +0000

--- 00013_Batalla/test..js
+++ 00013_Batalla/test..js
@@ -1,5 +1,7 @@
 describe("", function() {
   it("Si hacemos pelea({ nombre: 'Chun-Li', vida: 100, ataque: 40, defensa: 10 }, { nombre: 'Cammy', vida: 50, ataque: 20, defensa: 20 } retorna { rondas: 3, gano: 'Chun-Li'}", function() {
+    let combatienteA = { nombre: "Chun-Li", vida: 100, ataque: 40, defensa: 10 };
+    let combatienteB = { nombre: "Cammy", vida: 50, ataque: 20, defensa: 20 };
     let resultado =  pelea(combatienteA, combatienteB)
     assert.equal(resultado, { rondas: 3, gano: "Chun-Li"});
   })
@@ -7,6 +9,8 @@ describe("", function() {
 
 describe("", function() {
   it("Si hacemos pelea({ nombre: 'Bellota', vida: 100, ataque: 80, defensa: 5 }, { nombre: 'Mojo Jojo', vida: 60, ataque: 50, defensa: 30 } retorna { rondas: 2, gano: 'Bellota'}", function() {
+    let combatienteA = { nombre: 'Bellota', vida: 100, ataque: 80, defensa: 5 };
+    let combatienteB = { nombre: 'Mojo Jojo', vida: 60, ataque: 50, defensa: 30 };
     let resultado =  pelea(combatienteA, combatienteB)
     assert.equal(resultado, { rondas: 2, gano: "Bellota"});
   })
