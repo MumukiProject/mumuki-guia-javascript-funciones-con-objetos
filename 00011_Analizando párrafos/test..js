e1dbeb47b08cdf0a84e432c7eed95397a7e30d33
mayra@mumuki.org <mayra@mumuki.org>
2021-06-07 18:34:15 +0000

    Mumuki Export on 2021-06-07 18:34:15 +0000

--- 00011_Analizando párrafos/test..js
+++ 00011_Analizando párrafos/test..js
@@ -1,17 +1,20 @@
 describe("", function() {
-  let resultado =  obtenerInfoDelParrafo("Do. Or do not. There is no try.");
-  it("Si hacemos  obtenerInfoDelParrafo('Do. Or do not. There is no try.') la cantidad de letras es 21", function() {
+  it("Si hacemos  obtenerInfoDelParrafo('Do. Or do not. There is no try.') la cantidad de letras es 21",
+  function() {
+    let resultado =  obtenerInfoDelParrafo("Do. Or do not. There is no try.");
     assert.equal(21, resultado.letras);
   })
   it("Si hacemos  obtenerInfoDelParrafo('Do. Or do not. There is no try.') la cantidad de palabras es 8", function() {
+    let resultado =  obtenerInfoDelParrafo("Do. Or do not. There is no try.");
     assert.equal(resultado.palabras, 8);
   })
   it("Si hacemos  obtenerInfoDelParrafo('Do. Or do not. There is no try.') la cantidad de oraciones es 3", function() {
+    let resultado =  obtenerInfoDelParrafo("Do. Or do not. There is no try.");
     assert.equal(resultado.oraciones, 3);
   })
 });
 
-describe("", function() {
+/*describe("", function() {
   let resultado =  obtenerInfoDelParrafo("¿Mi nombre qué letras tiene?. ¿Acaso es el quien en pie me mantiene?");
   it("Si hacemos obtenerInfoDelParrafo('¿Mi nombre qué letras tiene?. ¿Acaso es el quien en pie me mantiene?') la cantidad de letras es 51", function() {
     assert.equal(resultado.letras, 51);
@@ -22,4 +25,4 @@ describe("", function() {
   it("Si hacemos obtenerInfoDelParrafo('¿Mi nombre qué letras tiene?. ¿Acaso es el quien en pie me mantiene?') la cantidad de letras es 51", function() {
     assert.equal(resultado.oraciones, 2);
   })
-});
+});*/
