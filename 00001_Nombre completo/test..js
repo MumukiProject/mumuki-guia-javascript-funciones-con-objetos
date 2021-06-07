1a702627e1688aeb897df00ae57190847c4fcdac
mayra@mumuki.org <mayra@mumuki.org>
2021-06-07 13:37:13 +0000

    Mumuki Export on 2021-06-07 13:37:13 +0000

--- 00001_Nombre completo/test..js
+++ 00001_Nombre completo/test..js
@@ -10,3 +10,29 @@ describe("", function() {
     assert.equal(resultado,'Valentina Tereshkova');
   })
 })
+
+describe("", function() {
+  const eddie = {
+  nombre: 'Eddie',
+  apellido: 'Vedder',
+  email: 'Eddie.Veder@gmail.com',
+  plan: 'premium'
+};
+  it("Si la función obtenerNombreCompleto, recibe por parámetro una persona que se llama Eddie Vedder, nos va a retornar Eddie Vedder", function() {
+    let resultado = obtenerNombreCompleto(eddie);
+    assert.equal(resultado,'Eddie Vedder');
+  })
+})
+
+describe("", function() {
+  const brenda = {
+  nombre: 'Brenda',
+  apellido: 'Martin',
+  email: 'Brenda.Martin@gmail.com',
+  plan: 'premium'
+};
+  it("Si la función obtenerNombreCompleto, recibe por parámetro una persona que se llama Brenda Martin, nos va a retornar Brenda MArtin", function() {
+    let resultado = obtenerNombreCompleto(eddie);
+    assert.equal(resultado,'Brenda Martin');
+  })
+})
