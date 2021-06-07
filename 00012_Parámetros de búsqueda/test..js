0f4b4eba06d9489d01bdaeb35e8ae751d9de4e47
mayra@mumuki.org <mayra@mumuki.org>
2021-06-07 18:20:09 +0000

    Mumuki Export on 2021-06-07 18:20:09 +0000

--- 00012_Parámetros de búsqueda/test..js
+++ 00012_Parámetros de búsqueda/test..js
@@ -10,3 +10,17 @@ describe("", function() {
     assert.equal(resultado.level, 'intermediate');
   })
 });
+
+describe("", function() {
+  let resultado =  parseQueryParams("http://www.exercises.com?keyword=functional&language=haskell&level=advanced");
+  it("Si hacemos  ('http://www.exercises.com?keyword=functional&language=haskell&level=advanced') el keyword es objects", function() {
+    assert.equal(resultado.keyword, 'functional');
+  })
+  it("Si hacemos  ('http://www.exercises.com?keyword=functional&language=haskell&level=advanced') el language es haskell", function() {
+    assert.equal(resultado.language, 'haskell');
+  })
+  it("Si hacemos  ('http://www.exercises.com?keyword=functional&language=haskell&level=advanced') el level es advanced", function() {
+    assert.equal(resultado.level, 'advanced');
+  })
+});
+
