80ec03d6c68e9d4d622beb6d1b96fc673bfd0e1e
mayra@mumuki.org <mayra@mumuki.org>
2021-06-07 20:33:24 +0000

    Mumuki Export on 2021-06-07 20:33:23 +0000

--- 00017_Eliminar propiedades/test..js
+++ 00017_Eliminar propiedades/test..js
@@ -1,1 +1,17 @@
-TODO
+describe("", function() {
+  it("Si hacemos removerPropiedades({ a: 1, b: 2, c: 3 }, ['c']), nos retorna {a: 1, b: 2}", function() {
+   let data = { a: 1, b: 2, c: 3 };
+   let propiedades = ['c'];
+   let resultado = removerPropiedades(data, propiedades)
+    assert.equal(resultado, {a: 1, d: 4});
+  })
+});
+
+describe("", function() {
+  it("Si hacemos removerPropiedades({ a: 10, b: 5, c: 8 }, ['a','c']), nos retorna {b: 2}", function() {
+   let data = { a: 1, b: 2, c: 3 };
+   let propiedades = ['a','c'];
+   let resultado = removerPropiedades(data, propiedades)
+    assert.equal(resultado, {b: 2});
+  })
+});
