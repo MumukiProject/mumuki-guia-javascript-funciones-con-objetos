9186801a0c311b9a5dea42e9f7dabaa0a87bfe20
mayra@mumuki.org <mayra@mumuki.org>
2021-06-07 17:09:53 +0000

    Mumuki Export on 2021-06-07 17:09:53 +0000

--- 00011_Analizando párrafos/test..js
+++ 00011_Analizando párrafos/test..js
@@ -1,25 +1,12 @@
 describe("", function() {
-  let resultado = obtenerInfoString('H3ll0 Wor1d');
-  it("Si hacemos obtenerInfoString('H3ll0 Wor1d') la cantidad de letras es 7", function() {
-    assert.equal(resultado.letras, 7);
+  let resultado =  obtenerInfoDelParrafo("Do. Or do not. There is no try.");
+  it("Si hacemos  obtenerInfoDelParrafo('Do. Or do not. There is no try.') la cantidad de letras es 21", function() {
+    assert.equal(resultado.letras, 21);
   })
-  it("Si hacemos obtenerInfoString('H3ll0 Wor1d') la cantidad de digitos es 3", function() {
-    assert.equal(resultado.digitos, 3);
+  it("Si hacemos  obtenerInfoDelParrafo('Do. Or do not. There is no try.') la cantidad de letras es 21", function() {
+    assert.equal(resultado.palabras, 8);
   })
-  it("Si hacemos obtenerInfoString('H3ll0 Wor1d') la cantidad de espacios es 1", function() {
-    assert.equal(resultado.espacios, 1);
-  })
-});
-
-describe("", function() {
-  let resultado = obtenerInfoString('H0L4 4D4, B13NV3N1D4');
-  it("Si hacemos obtenerInfoString('H0L4 4D4, B13NV3N1D4') la cantidad de letras es 8", function() {
-    assert.equal(resultado.letras, 8);
-  })
-  it("Si hacemos obtenerInfoString('H0L4 4D4, B13NV3N1D4') la cantidad de digítos es 10", function() {
-    assert.equal(resultado.digitos, 10);
-  })
-  it("Si hacemos obtenerInfoString('H0L4 4D4, B13NV3N1D4') la cantidad de espacios es 2", function() {
-    assert.equal(resultado.espacios, 2);
+  it("Si hacemos  obtenerInfoDelParrafo('Do. Or do not. There is no try.') la cantidad de oraciones es 3", function() {
+    assert.equal(resultado.oraciones, 3);
   })
 });
