6662ea5e758e0640a3154d86161f86a0c7af083e
mayra@mumuki.org <mayra@mumuki.org>
2021-06-07 13:32:38 +0000

    Mumuki Export on 2021-06-07 13:32:38 +0000

--- 00001_Nombre completo/test..js
+++ 00001_Nombre completo/test..js
@@ -1,7 +1,12 @@
-/*describe("", function() {
-  it("Si ", function() {
-    perfil.nombre = 'Juli';
-    perfil.edad = 48;
-    assert.equal(perfil.presentacion(),'Hola, mi nombre es Juli y tengo 48 años');
+describe("", function() {
+  const valentina = {
+  nombre: 'Valentina',
+  apellido: 'Tereshkova',
+  email: 'Valentina.Tereshkova@gmail.com',
+  plan: 'premium'
+};
+  it("Si la función obtenerNombreCompleto, recibe por parámetro una persona que se llama Valentina Tereshkova, nos va a retornar Valentina Tereshkova", function() {
+    let resultado = obtenerNombreCompleto(valentina);
+    assert.equal(resultado,'Valentina Tereshkova');
   })
-})*/
+})
