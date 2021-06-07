4f599b7d21c37d5c040b25fb1d286de6817f7a33
mayra@mumuki.org <mayra@mumuki.org>
2021-06-07 15:35:23 +0000

    Mumuki Export on 2021-06-07 15:35:22 +0000

--- 00009_Obtener experiencia/test..js
+++ 00009_Obtener experiencia/test..js
@@ -1,7 +1,7 @@
 describe("", function() {
   const puntajes = { EASY: 10, MEDIUM: 50, HARD: 100 };
   const desafios = { EASY: 3, MEDIUM: 4, HARD: 2 };
-  it("Si hacemos obtenerExperiencia(puntajes, desafios) retorna 430", function() {
+  it("Si hacemos obtenerExperiencia({ EASY: 10, MEDIUM: 50, HARD: 100 }, { EASY: 3, MEDIUM: 4, HARD: 2 }) retorna 430", function() {
     let resultado = obtenerExperiencia(puntajes, desafios);
     assert.equal(resultado, 430);
   })
@@ -10,7 +10,7 @@ describe("", function() {
 describe("", function() {
   const puntajes = { EASY: 10, MEDIUM: 50, HARD: 100 };
   const desafios = { EASY: 1, MEDIUM: 2, HARD: 5 };
-  it("Si hacemos obtenerExperiencia(puntajes, EASY: 1, MEDIUM: 2, HARD: 5) retorna 610", function() {
+  it("Si hacemos obtenerExperiencia({ EASY: 10, MEDIUM: 50, HARD: 100 }, {EASY: 1, MEDIUM: 2, HARD: 5}) retorna 610", function() {
     let resultado = obtenerExperiencia(puntajes, desafios);
     assert.equal(resultado, 610);
   })
@@ -21,6 +21,6 @@ describe("", function() {
   const desafios = { EASY: 3, MEDIUM: 4, HARD: 2 };
   it("Si hacemos obtenerExperiencia({ EASY: 1, MEDIUM: 2, HARD: 3 }, {EASY: 1, MEDIUM: 2, HARD: 5}) retorna 17", function() {
     let resultado = obtenerExperiencia(puntajes, desafios);
-    assert.equal(17);
+    assert.equal(resultado,17);
   })
 });
