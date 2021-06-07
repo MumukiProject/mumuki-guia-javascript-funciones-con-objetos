001faed20dd8354c5163a0c4838afd4198538734
mayra@mumuki.org <mayra@mumuki.org>
2021-06-07 14:36:24 +0000

    Mumuki Export on 2021-06-07 14:36:24 +0000

--- 00005_Volumen de caja/test..js
+++ 00005_Volumen de caja/test..js
@@ -1,6 +1,5 @@
-/*describe("", function() {
-  const caja1 = { width: 4, length: 8, height: 5 };
-};
+describe("", function() {
+  const caja1 = { width: 4, length: 8, height: 5};
   it("Si hacemos volumenCaja de una caja que tiene width: 4, length: 8, height: 5, nos retorna 160", function() {
     let resultado = volumenCaja(caja1);
     assert.equal(resultado, 160);
@@ -8,8 +7,7 @@
 });
 
 describe("", function() {
-  const caja2 = { width: 2, length: 5, height: 1 };
-};
+  const caja2 = { width: 2, length: 5, height: 1};
   it("Si hacemos volumenCaja de una caja que tiene width: 2, length: 5, height: 1, nos retorna 10", function() {
     let resultado = volumenCaja(caja2);
     assert.equal(resultado, 10);
@@ -17,10 +15,9 @@ describe("", function() {
 });
 
 describe("", function() {
-  const caja3 = { width: 10, length: 10, height: 10 };
-};
+  const caja3 = { width: 10, length: 10, height: 10};
   it("Si hacemos volumenCaja de una caja que tiene width: 10, length: 10, height: 10, nos retorna 1000", function() {
     let resultado = volumenCaja(caja3);
     assert.equal(resultado, 1000);
   })
-});*/
+});
