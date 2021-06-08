8f620fa09fd2946fd4ae24f054e945303eab3c1f
mayra@mumuki.org <mayra@mumuki.org>
2021-06-08 13:19:24 +0000

    Mumuki Export on 2021-06-08 13:19:24 +0000

--- 00018_Filtrar propiedades/test..js
+++ 00018_Filtrar propiedades/test..js
@@ -2,7 +2,7 @@ describe("", function() {
   it("Si hacemos filtrarPropiedades({ a: 1, b: 2, c: 3 }, ['c', 'b']), nos retorna {b: 2, c: 3}", function() {
    let data = { a: 1, b: 2, c: 3 };
     let props = ["c", "b"];
-   let resultado = filtrarPropiedades(data, propiedades)
+   let resultado = filtrarPropiedades(data, props)
     assert.equal(resultado, {b: 2, c: 3});
   })
 });
