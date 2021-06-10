dc78f6b969a0be43acce66832b1e03f98036a270
gustavo@mumuki.org <gustavo@mumuki.org>
2021-06-10 14:28:23 +0000

    Mumuki Export on 2021-06-10 14:28:23 +0000

--- 00004_Render álbum/test..js
+++ 00004_Render álbum/test..js
@@ -9,7 +9,7 @@ describe("", function() {
   info: 'https://en.wikipedia.org/wiki/With_the_Lights_Out'
 };
   it("Si tenemos un disco de Nirvana, al hacer render(albumNirvana), el HTML se renderiza con el disco de Nirvana'", function() {
-    let esperado = "\n <div class=\"card m-5\" id=\"nirv1234\">\n <img class=\"card-img-top\" src=\"https://muzikalia.com/wp-content/uploads/2005/03/nirvana__with_the_lights_out.jpg\" alt=\"Nirvana - With The Lights Out\" />\n <div class=\"card-body\">\n <h5 class=\"card-title\">Nirvana</h5>\n <p class=\"card-text\">With The Lights Out (2004) | Grunge</p>\n <a href=\"https://en.wikipedia.org/wiki/With_the_Lights_Out\" class=\"btn btn-primary\">https://en.wikipedia.org/wiki/With_the_Lights_Out</a>\n </div>\n </div>\n ";
+    let esperado = "\n <div class=\"card m-5\" id=\"nirv1234\">\n <img class=\"card-img-top\" src=\"https://muzikalia.com/wp-content/uploads/2005/03/nirvana__with_the_lights_out.jpg\" alt=\"Nirvana - With The Lights Out\" />\n <div class=\"card-body\">\n <h5 class=\"card-title\">Nirvana</h5>\n <p class=\"card-text\">With The Lights Out (2004) | Grunge</p>\n <a href=\"https://en.wikipedia.org/wiki/With_the_Lights_Out\" class=\"btn btn-primary\">https://en.wikipedia.org/wiki/With_the_Lights_Out</a>\n </div>\n </div>\n";
     let resultado = render(albumNirvana);
     assert.equal(resultado, esperado);
   })
