63b091d853c4908780484c2d96bda52a81be3a3e
gustavo@mumuki.org <gustavo@mumuki.org>
2021-06-10 14:33:33 +0000

    Mumuki Export on 2021-06-10 14:33:33 +0000

--- 00004_Render álbum/test..js
+++ 00004_Render álbum/test..js
@@ -11,5 +11,6 @@ describe("", function() {
   it("Si tenemos un disco de Nirvana, al hacer render(albumNirvana), el HTML se renderiza con el disco de Nirvana'", function() {
     let resultado = render(albumNirvana);
     assert(resultado.includes("id='nirv1234'") || resultado.includes(`id="nirv1234"`))
+    assert(resultado.includes("src='https://muzikalia.com/wp-content/uploads/2005/03/nirvana__with_the_lights_out.jpg'") || resultado.includes(`src="https://muzikalia.com/wp-content/uploads/2005/03/nirvana__with_the_lights_out.jpg"`))
   })
 });
