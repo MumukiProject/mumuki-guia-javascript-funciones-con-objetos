af4523ab3d3547563cd7b463e9bbb1682fcfef30
mayra@mumuki.org <mayra@mumuki.org>
2021-06-10 17:27:06 +0000

    Mumuki Export on 2021-06-10 17:27:06 +0000

--- 00004_Render álbum/test..js
+++ 00004_Render álbum/test..js
@@ -18,6 +18,30 @@ describe("", function() {
   });
   it("Si tenemos un disco de Nirvana con genero = 'Grunge', el HTML se renderiza con ese genero", function() {
     let resultado = render(albumNirvana);
-    assert(resultado.includes("Grunge'") || resultado.includes(`Grunge`));
+    assert(resultado.includes("Grunge") || resultado.includes(`Grunge`));
+  })
+});
+
+describe("", function() {
+  let albumPinkFloyd= {
+  id: 'PinkFloyd',
+  nombre: 'Wish You Were Here',
+  anio: 1975,
+  genero: 'Soft Rock',
+  banda: 'Pink Floyd',
+  portada: 'https://i0.wp.com/hipertextual.com/wp-content/uploads/2017/12/whis-you-were-here-foto-original.jpg?resize=670%2C848&ssl=1',
+  info: 'https://es.wikipedia.org/wiki/Wish_You_Were_Here'
+};
+  it("Si tenemos un disco de Pink Floyd con nombre = 'Wish You Were Here', al hacer render(albumPinkFloyd), el HTML se renderiza con ese nombre'", function() {
+    let resultado = render(albumPinkFloyd);
+    assert(resultado.includes("Wish You Were Here") || resultado.includes(`Wish You Were Here"`))
+  });
+  it("Si tenemos un disco de Pink Floyd con genero = 'Soft Rock', al hacer render(albumPinkFloyd), el HTML se renderiza con ese genero '", function() {
+    let resultado = render(albumPinkFloyd);
+    assert(resultado.includes("Soft Rock") || resultado.includes(`Soft Rock`))
+  });
+  it("Si tenemos un disco de Pink Floyd con info = 'https://es.wikipedia.org/wiki/Wish_You_Were_Here', el HTML se renderiza con esa información", function() {
+    let resultado = render(albumPinkFloyd);
+    assert(resultado.includes("https://es.wikipedia.org/wiki/Wish_You_Were_Here") || resultado.includes(`https://es.wikipedia.org/wiki/Wish_You_Were_Here`));
   })
 });
