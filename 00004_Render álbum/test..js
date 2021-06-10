9e09c6421467436cdc005f867c62ebed1bb0129d
gustavo@mumuki.org <gustavo@mumuki.org>
2021-06-10 14:24:17 +0000

    Mumuki Export on 2021-06-10 14:24:17 +0000

--- 00004_Render álbum/test..js
+++ 00004_Render álbum/test..js
@@ -9,7 +9,17 @@ describe("", function() {
   info: 'https://en.wikipedia.org/wiki/With_the_Lights_Out'
 };
   it("Si tenemos un disco de Nirvana, al hacer render(albumNirvana), el HTML se renderiza con el disco de Nirvana'", function() {
+    let esperado = `
+    <div class="card m-5" id="nirv1234">
+      <img class="card-img-top" src="https://muzikalia.com/wp-content/uploads/2005/03/nirvana__with_the_lights_out.jpg" alt="Nirvana - With The Lights Out" />
+      <div class="card-body">
+        <h5 class="card-title">Nirvana</h5>
+        <p class="card-text">With The Lights Out (2004) | Grunge</p>
+        <a href="https://en.wikipedia.org/wiki/With_the_Lights_Out" class="btn btn-primary">https://en.wikipedia.org/wiki/With_the_Lights_Out</a>
+      </div>
+    </div>
+    `;
     let resultado = render(albumNirvana);
-    assert.equal(resultado,"<div class='card m-5' id='nirv1234'><img class='card-img-top' src='https://muzikalia.com/wp-content/uploads/2005/03/nirvana__with_the_lights_out.jpg' alt='Nirvana - With The Lights Out' /><div class='card-body'><h5 class='card-title'>Nirvana</h5><p class='card-text'>With The Lights Out (2004) | Grunge</p><a href='https://en.wikipedia.org/wiki/With_the_Lights_Out' class='btn btn-primary'>https://en.wikipedia.org/wiki/With_the_Lights_Out</a></div></div>");
+    assert.equal(resultado, esperado);
   })
 });
