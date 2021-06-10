6f1c0967b33ed99c181f39a7f0a6c21e1fd6e351
mayra@mumuki.org <mayra@mumuki.org>
2021-06-10 17:17:12 +0000

    Mumuki Export on 2021-06-10 17:17:12 +0000

--- 00004_Render álbum/test..js
+++ 00004_Render álbum/test..js
@@ -8,9 +8,16 @@ describe("", function() {
   portada: 'https://muzikalia.com/wp-content/uploads/2005/03/nirvana__with_the_lights_out.jpg',
   info: 'https://en.wikipedia.org/wiki/With_the_Lights_Out'
 };
-  it("Si tenemos un disco de Nirvana, al hacer render(albumNirvana), el HTML se renderiza con el disco de Nirvana'", function() {
+  it("Si tenemos un disco de Nirvana con id = 'nirv1234', al hacer render(albumNirvana), el HTML se renderiza con ese id '", function() {
     let resultado = render(albumNirvana);
     assert(resultado.includes("id='nirv1234'") || resultado.includes(`id="nirv1234"`))
+  });
+  it("Si tenemos un disco de Nirvana con portada = 'https://muzikalia.com/wp-content/uploads/2005/03/nirvana__with_the_lights_out.jpg', al hacer render(albumNirvana), el HTML se renderiza con ese src '", function() {
+    let resultado = render(albumNirvana);
     assert(resultado.includes("src='https://muzikalia.com/wp-content/uploads/2005/03/nirvana__with_the_lights_out.jpg'") || resultado.includes(`src="https://muzikalia.com/wp-content/uploads/2005/03/nirvana__with_the_lights_out.jpg"`))
+  });
+  it("Si tenemos un disco de Nirvana con genero = 'Grunge', el HTML se renderiza con ese genero", function() {
+    let resultado = render(albumNirvana);
+    assert(resultado.includes("Grunge'") || resultado.includes(`Grunge`));
   })
 });
