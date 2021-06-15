05df21d2d587db73cd68a5250c6df9da7e1d771f
gustavo@mumuki.org <gustavo@mumuki.org>
2021-06-15 20:00:23 +0000

    Mumuki Export on 2021-06-15 20:00:23 +0000

--- 00006_Redondeo/test..js
+++ 00006_Redondeo/test..js
@@ -1,20 +1,20 @@
 describe("", function() {
   it("Si hacemos redondear(8.4) nos devuelve { floor: 8, ceil: 9, round: 8 }", function() {
     let resultado = redondear(8.4);
-    assert.equal(resultado, { floor: 8, ceil: 9, round: 8 });
+    assert.deepEqual(resultado, { floor: 8, ceil: 9, round: 8 });
   })
 });
 
 describe("", function() {
   it("Si hacemos redondear(5.9) nos devuelve { floor: 5, ceil: 6, round: 6 }", function() {
     let resultado = redondear(5.9);
-    assert.equal(resultado, { floor: 5, ceil: 6, round: 6 });
+    assert.deepEqual(resultado, { floor: 5, ceil: 6, round: 6 });
   })
 });
 
 describe("", function() {
   it("Si hacemos redondear(24.1) nos devuelve { floor: 24, ceil: 25, round: 24 }", function() {
     let resultado = redondear(24.1);
-    assert.equal(resultado, { floor: 24, ceil: 25, round: 24 });
+    assert.deepEqual(resultado, { floor: 24, ceil: 25, round: 24 });
   })
 });
