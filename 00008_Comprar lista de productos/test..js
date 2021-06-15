d3cdd4218739962b918b22ed2015b4106e59b6d1
gustavo@mumuki.org <gustavo@mumuki.org>
2021-06-15 20:13:15 +0000

    Mumuki Export on 2021-06-15 20:13:15 +0000

--- 00008_Comprar lista de productos/test..js
+++ 00008_Comprar lista de productos/test..js
@@ -1,6 +1,6 @@
 describe("", function() {
   let productos = { mouse: 500, teclado: 1000, monitor: 3000 };
-  it("Si hacemos puedoComprarTodos(4500, 'mouse: 500, teclado: 1000, monitor: 3000', nos retorna true", function() {
+  it("Si hacemos puedoComprarTodos(4500, { mouse: 500, teclado: 1000, monitor: 3000 }, nos retorna true", function() {
     let resultado = puedoComprarTodos(4500, productos);
     assert(resultado);
   })
@@ -8,15 +8,15 @@ describe("", function() {
 
 describe("", function() {
   let productos1 = { remera: 1000, zapatillas: 5000, pantalon: 1500 };
-  it("Si hacemos puedoComprarTodos(5000, 'remera: 1000, zapatillas: 5000, pantalon: 1500', nos retorna false", function() {
+  it("Si hacemos puedoComprarTodos(5000, { remera: 1000, zapatillas: 5000, pantalon: 1500 }, nos retorna false", function() {
     let resultado = puedoComprarTodos(5000, productos1);
-    assert.equal(resultado, false);
+    assert(!resultado);
   })
 });
 
 describe("", function() {
   let productos2 = { fideos: 40, salsa: 45, cebolla: 30 };
-  it("Si hacemos puedoComprarTodos(150, 'fideos: 40, salsa: 45, cebolla: 30', nos retorna true", function() {
+  it("Si hacemos puedoComprarTodos(150, { fideos: 40, salsa: 45, cebolla: 30 }, nos retorna true", function() {
     let resultado = puedoComprarTodos(40, productos2);
     assert(resultado);
   })
