a22fa51b180c8fbacde43c946714674f98758942
gustavo@mumuki.org <gustavo@mumuki.org>
2021-06-15 19:24:03 +0000

    Mumuki Export on 2021-06-15 19:24:03 +0000

--- 00002_Estado estudiantes/test..js
+++ 00002_Estado estudiantes/test..js
@@ -4,9 +4,9 @@ describe("", function() {
     algoritmos: [10, 7, 9],
     algebra: [4, 5]
   };
-  it("Si Agus aprobó todas las materias, al hacer agruparMateriasPorEstado(materiasAgus) muestra aprobadas: ['matematica', 'algoritmos', 'algebra'], desaprobadas: []", function() {
+  it("Si Agus aprobó todas las materias, al hacer agruparMateriasPorEstado(materiasAgus) nos devuelve { aprobadas: ['matematica', 'algoritmos', 'algebra'], desaprobadas: [] }", function() {
     let resultado = agruparMateriasPorEstado(materiasAgus);
-    assert.equal(resultado,"aprobadas: ['matematica', 'algoritmos', 'algebra'], desaprobadas: []");
+    assert.equal(resultado,"{ aprobadas: ['matematica', 'algoritmos', 'algebra'], desaprobadas: []} ");
   })
 });
 
@@ -16,9 +16,9 @@ describe("", function() {
     algoritmos: [2, 4, 2],
     algebra: [4, 3]
   };
-  it("Si Juli desaprobó todas las materias, al hacer agruparMateriasPorEstado(materiasJuli) muestra aprobadas: [], desaprobadas: ['matematica', 'algoritmos', 'algebra']", function() {
+  it("Si Juli desaprobó todas las materias, al hacer agruparMateriasPorEstado(materiasJuli) nos devuelve { aprobadas: [], desaprobadas: ['matematica', 'algoritmos', 'algebra'] }", function() {
     let resultado = agruparMateriasPorEstado(materiasJuli);
-    assert.equal(resultado,"aprobadas: [], desaprobadas: ['matematica', 'algoritmos', 'algebra']");
+    assert.equal(resultado,{ "aprobadas: [], desaprobadas: ['matematica', 'algoritmos', 'algebra']" });
   })
 });
 
@@ -28,8 +28,8 @@ describe("", function() {
     algoritmos: [4, 4, 4],
     algebra: [4, 4]
   };
-  it("Si Ale desaprobó matematica, al hacer agruparMateriasPorEstado(materiasAle) muestra aprobadas: ['algoritmos', 'algebra'], desaprobadas: ['matematica']", function() {
+  it("Si Ale desaprobó matematica, al hacer agruparMateriasPorEstado(materiasAle) nos devuelve { aprobadas: ['algoritmos', 'algebra'], desaprobadas: ['matematica'] }", function() {
     let resultado = agruparMateriasPorEstado(materiasJuli);
-    assert.equal(resultado,"aprobadas: ['algoritmos', 'algebra'], desaprobadas: ['matematica']");
+    assert.equal(resultado,"{ aprobadas: ['algoritmos', 'algebra'], desaprobadas: ['matematica'] }");
   })
 });
