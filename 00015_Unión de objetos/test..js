5e7cc01eae4ddaf62d88adebff7827fd3f430082
gustavo@mumuki.org <gustavo@mumuki.org>
2021-06-16 16:23:52 +0000

    Mumuki Export on 2021-06-16 16:23:52 +0000

--- 00015_Unión de objetos/test..js
+++ 00015_Unión de objetos/test..js
@@ -3,7 +3,7 @@ describe("", function() {
    let info1 = { a: 1, b: 2, c: 3 };
    let info2 = { d: 4, b: 5 };
    let resultado = merge(info1, info2)
-    assert.equal(resultado, {a: 1, b: 2, c: 3, d: 4});
+    assert.deepEqual(resultado, {a: 1, b: 2, c: 3, d: 4});
   })
 });
 
@@ -12,6 +12,6 @@ describe("", function() {
    let info1 = { a: 4, b: 8, c: 5 };
    let info2 = { h: 4, b: 5 };
    let resultado = merge(info1, info2)
-    assert.equal(resultado, {a: 4, b: 8, c: 5, h: 4});
+    assert.deepEqual(resultado, {a: 4, b: 8, c: 5, h: 4});
   })
 });
