933f6d25650b3fd5f95aca893c7d80928f13ad71
gustavo@mumuki.org <gustavo@mumuki.org>
2021-06-16 16:30:57 +0000

    Mumuki Export on 2021-06-16 16:30:57 +0000

--- 00016_Diferencia de objetos/test..js
+++ 00016_Diferencia de objetos/test..js
@@ -1,5 +1,5 @@
 describe("", function() {
-  it("Si hacemos diferencia({ a: 1, b: 2, c: 3 }, { d: 4, b: 5 })", function() {
+  it("Si hacemos diferencia({ a: 1, b: 2, c: 3 }, { d: 4, b: 5 }) retorna {a: 1, c: 3, d: 4}", function() {
    let info1 = { a: 1, b: 2, c: 3 };
    let info2 = { d: 4, b: 5 };
    let resultado = diferencia(info1, info2)
@@ -8,10 +8,28 @@ describe("", function() {
 });
 
 describe("", function() {
-  it("Si hacemos diferencia({ a: 4, b: 8, c: 5 }, { h: 4, b: 5 })", function() {
+  it("Si hacemos diferencia({ a: 4, b: 8, c: 5 }, { h: 4, b: 5 }) retorna {a: 4, c: 5, h: 4}", function() {
    let info1 = { a: 4, b: 8, c: 5 };
    let info2 = { h: 4, b: 5 };
    let resultado = diferencia(info1, info2)
     assert.equal(resultado, {a: 4, c: 5, h: 4});
+  })
+});
+
+describe("", function() {
+  it("Si hacemos diferencia({ a: 4, b: 8, c: 5 }, { d: 4, e: 5 }) retorna {a: 4, b: 8, c: 5, d: 4, e: 5 }", function() {
+   let info1 = { a: 4, b: 8, c: 5 };
+   let info2 = { d: 4, e: 5 };
+   let resultado = diferencia(info1, info2)
+    assert.equal(resultado, {a: 4, b: 8, c: 5, d: 4, e: 5 });
+  })
+});
+
+describe("", function() {
+  it("Si hacemos diferencia({ a: 4, b: 8, c: 5 }, { a: 4, b: 8, c: 5 ) retorna {}", function() {
+   let info1 = { a: 4, b: 8, c: 5 };
+   let info2 = { a: 4, b: 8, c: 5 ;
+   let resultado = diferencia(info1, info2)
+    assert.equal(resultado, {});
   })
 });
