b771e4a680c8b7be65d12eb030f5cdbfa2740cb8
gustavo@mumuki.org <gustavo@mumuki.org>
2021-06-16 16:36:08 +0000

    Mumuki Export on 2021-06-16 16:36:08 +0000

--- 00017_Eliminar propiedades/test..js
+++ 00017_Eliminar propiedades/test..js
@@ -3,7 +3,7 @@ describe("", function() {
    let data = { a: 1, b: 2, c: 3 };
    let propiedades = ['c'];
    let resultado = removerPropiedades(data, propiedades)
-    assert.equal(resultado, {a: 1, d: 4});
+    assert.deepEqual(resultado, {a: 1, d: 4});
   })
 });
 
@@ -12,6 +12,6 @@ describe("", function() {
    let data = { a: 1, b: 2, c: 3 };
    let propiedades = ['a','c'];
    let resultado = removerPropiedades(data, propiedades)
-    assert.equal(resultado, {b: 2});
+    assert.deepEqual(resultado, {b: 2});
   })
 });
